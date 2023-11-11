0cf478b172f083e5edeb15ea6eff7628759bfb6d
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2023-11-11 14:12:00 +0100

    Allow to render simple lines and vertican and horizontal rules

--- viz.js
+++ viz.js
@@ -7,6 +7,65 @@ const createNode = (tag, parent) => {
     return element;
 };
 
+// Create a path
+const createPath = () => {
+    const path = [];
+    return {
+        // Move the current point to the coordinate x,y
+        move: (x, y) => path.push(`M${x},${y}`),
+        // Draw a line from the current point to the end point specified by x,y
+        line: (x, y) => path.push(`L${x},${y}`),
+        // Draw a horizontal line from the current point to the end point
+        hLine: x => path.push(`H${x}`),
+        // Draw a vertical line from the current point to the end point
+        vLine: y => path.push(`V${y}`),
+        // Draw an arc from the current point to the specified point
+        // rx and ry are the two radius of the ellipse
+        // angle represents a rotation (in degree) of the ellipse relative to the x-axis
+        // laf (large-arc-flag) allows to chose one of the large arc (1) or small arc (0)
+        // sf (sweep-flag) allows to chose one of the clockwise turning arc (1) or anticlockwise turning arc (0)
+        // x and y become the new current point for the next command
+        // Documentation: https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/d#Elliptical_Arc_Curve 
+        arc: (rx, ry, angle, laf, sf, x, y) => {
+            path.push(`A${rx},${ry},${angle},${laf},${sf},${x},${y}`);
+        },
+        // Draw a quadratic Bézier curve from the current point to the end point specified by x,y
+        // The control point is specified by x1,y1 
+        // Documentation: https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/d#Quadratic_B%C3%A9zier_Curve 
+        quadraticCurve: (x1, y1, x, y) => {
+            path.push(`Q${x1},${y1},${x},${y}`);
+        },
+        // Draw a cubic Bézier curve from the current point to the end point specified by x,y
+        // The start control point is specified by x1,y1
+        // The end control point is specified by x2,y2
+        // Documentation: https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/d#Cubic_B%C3%A9zier_Curve
+        bezierCurve: (x1, y1, x2, y2, x, y) => {
+            path.push(`C${x1},${y1},${x2},${y2},${x},${y}`);
+        },
+        // Close the current path
+        close: () => path.push("Z"),
+        // Get full path
+        toString: () => path.join(" "),
+    };
+};
+
+// Create a polyline
+const createPolyline = (points, closed = false) => {
+    const path = createPath();
+    if (points && points.length > 0) {
+        // Move to the first point of the polyline
+        path.move(points[0][0], points[0][1]);
+        for (let i = 1; i < points.length; i++) {
+            path.line(points[i][0], points[i][1]);
+        }
+        // Check for closing the path
+        if (closed) {
+            path.close();
+        }
+    }
+    return path.toString();
+};
+
 // Get the value from a data
 const getValueOf = (getValue, datum, index, defaultValue = null) => {
     if (typeof getValue === "function") {
@@ -63,6 +122,51 @@ const textGeom = (data, options = {}) => {
     };
 };
 
+// Simple line geom
+const lineGeom = (data, options = {}) => {
+    return parent => {
+        return buildGeom(data, options, (datum, index, opt) => {
+            const element = createNode("path", parent);
+            const path = createPolyline([
+                [getValueOf(opt.x1, datum, index, 0), getValueOf(opt.y1, datum, index, 0)],
+                [getValueOf(opt.x2, datum, index, 0), getValueOf(opt.y2, datum, index, 0)],
+            ]);
+            element.setAttribute("d", path);
+            element.setAttribute("fill", "none"); // Prevent filled lines
+            element.setAttribute("stroke", getValueOf(opt.strokeColor, datum, index, "#000"));
+            element.setAttribute("strong-width", getValueOf(opt.strokeWidth, datum, index, 1));
+        });
+    };
+};
+
+// Horizontal rule geom
+const xRuleGeom = (data, options = {}) => {
+    return (parent, draw) => {
+        return buildGeom(data, options, (datum, index, opt) => {
+            const element = createNode("path", parent);
+            const y = getValueOf(opt.y, datum, index, 0);
+            element.setAttribute("d", createPolyline([[0, y], [draw.width, y]]));
+            element.setAttribute("fill", "none"); // Prevent filled lines
+            element.setAttribute("stroke", getValueOf(opt.strokeColor, datum, index, "#000"));
+            element.setAttribute("strong-width", getValueOf(opt.strokeWidth, datum, index, 1));
+        });
+    };
+};
+
+// Vertical rule geom
+const yRuleGeom = (data, options = {}) => {
+    return (parent, draw) => {
+        return buildGeom(data, options, (datum, index, opt) => {
+            const element = createNode("path", parent);
+            const x = getValueOf(opt.x, datum, index, 0);
+            element.setAttribute("d", createPolyline([[x, 0], [x, draw.height]]));
+            element.setAttribute("fill", "none"); // Prevent filled lines
+            element.setAttribute("stroke", getValueOf(opt.strokeColor, datum, index, "#000"));
+            element.setAttribute("strong-width", getValueOf(opt.strokeWidth, datum, index, 1));
+        });
+    };
+};
+
 // Generate a simple plot
 const createPlot = (options = {}, parent = null) => {
     const scene = createNode("svg", parent);
@@ -85,8 +189,12 @@ const createPlot = (options = {}, parent = null) => {
 
 export default {
     plot: createPlot,
+    path: createPath,
     geoms: {
         text: textGeom,
         point: pointGeom,
+        line: lineGeom,
+        xRule: xRuleGeom,
+        yRule: yRuleGeom,
     },
 };
